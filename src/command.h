5a8232279c854626a13b77ae5d4116ce90248211
antovic <my-email>
2023-12-30 21:51:37 +0100

    Updated server handles

--- src/command.h
+++ src/command.h
@@ -7,15 +7,15 @@ typedef enum {LOGIN = 1, LOGOUT, SEND, CHECK, RECEIVE, EXIT} Command;
 Command stringToCommand(char input[])
 {  
     Command ret = EXIT;
-    if(strcmp(input, "LOGIN"))
+    if(strcmp(input, "LOGIN") == 0)
         ret = LOGIN;
-    else if(strcmp(input, "LOGOUT"))
+    else if(strcmp(input, "LOGOUT") == 0)
         ret = LOGOUT;
-    else if(strcmp(input, "SEND"))
+    else if(strcmp(input, "SEND") == 0)
         ret = SEND;
-    else if(strcmp(input, "CHECK"))
+    else if(strcmp(input, "CHECK") == 0)
         ret = CHECK;
-    else if(strcmp(input, "RECEIVE"))
+    else if(strcmp(input, "RECEIVE") == 0)
         ret = RECEIVE;
     return ret; 
 }
--- src/server.c
+++ src/server.c
@@ -34,13 +34,168 @@
 typedef struct userData
 {
 	int *socket;
+    char username[12];
+    int loggedIn;
 } userData;
+
+int handleLogin(char *clientMessage, userData *userInfo)
+{
+    FILE* users = fopen("../files/users.txt", "r");
+    if(users == NULL)
+    {
+        printf("Encountered an error while open a file users.txt\n");
+        return 0;
+    }
+    if(userInfo->loggedIn == 1)
+    {
+        printf("Already logged in. Logout to proceed\n");
+        return 0;
+    }
+    char username[12], password[12], clientUsername[12], clientPassword[12];
+    strtok(clientMessage, " ");
+    strcpy(clientUsername, strtok(NULL, " "));
+    strcpy(clientPassword, strtok(NULL, " \n"));
+    while(fscanf(users, "%s %s", username, password) == 2)
+    {
+        if((strcmp(username, clientUsername) == 0) && (strcmp(password, clientPassword) == 0))
+        {
+            strcpy(userInfo->username, username);
+            userInfo->loggedIn = 1;
+            return 1;
+        }
+    }
+    printf("Wrong username or password, try again.\n");
+    // TODO: sendResponse(userInfo->socket, asjdiaspodjas, strlen(poruka))
+    fclose(users);
+    return 0;
+}
+
+int handleLogout(userData *userInfo)
+{
+    if(userInfo->loggedIn == 0)
+    {
+        printf("You are not logged in.");
+        return 0;
+    }
+    else
+    {
+        userInfo->username[0] = '\0';
+        userInfo->loggedIn = 0; 
+        return 1;
+    }
+}
+
+int handleSend(char *clientMessage, userData *userInfo)
+{
+    int found = 0;
+    char username[12], password[12], recipient[12], message[128], mailboxPath[128];
+    FILE *mailbox, *users;
+    if(userInfo->loggedIn == 0)
+    {
+        printf("You are not logged in.");
+        return 0;
+    }
+    strtok(clientMessage, " ");
+    strcpy(recipient, strtok(NULL, " "));
+    strcpy(message, strtok(NULL, "\n"));
+    users = fopen("../files/users.txt", "r");
+    if(users == NULL)
+    {
+        printf("Encountered while checking if user %s exists\n", recipient);
+        return 0;
+    }
+    while(fscanf(users, "%s %s", username, password) == 2)
+    {
+        if(strcmp(username, recipient) == 0)
+        {
+            found = 1;
+        }
+    }
+    if(found == 0)
+    {
+        printf("User does not exist.\n");
+        return 0;
+    }
+    strcpy(mailboxPath, "../files/");
+    strcat(mailboxPath, recipient);
+    strcat(mailboxPath, ".txt");
+    //printf("%s\n", mailboxPath); for debug
+    mailbox = fopen(mailboxPath, "a+");
+    if(mailbox == NULL)
+    {
+        printf("Encountered an error while accessing the mailbox. Please try again later.\n");
+        return 0;
+    }
+    fprintf(mailbox, "%s: %s\n", userInfo->username, message);
+    fclose(users);
+    fclose(mailbox);
+    return 1;
+}
+
+int handleCheck(userData* userInfo)
+{
+    char mailboxPath[128];
+    FILE *mailbox;
+    if(userInfo->loggedIn == 0)
+    {
+        printf("You are not logged in.");
+        return 0;
+    }
+    strcpy(mailboxPath, "../files/");
+    strcat(mailboxPath, userInfo->username);
+    strcat(mailboxPath, ".txt");
+    mailbox = fopen(mailboxPath, "r");
+    if(mailbox == NULL)
+    {
+        printf("Mailbox is either empty or unavailable at the moment. Please try again later.\n");
+        return 0;
+    }
+    else
+    {
+        printf("%s has some messages\n", userInfo->username);
+    }
+    fclose(mailbox);
+    return 1;
+}
+
+int handleReceive(userData* userInfo)
+{
+    char mailboxPath[128], content[256];
+    FILE *mailbox;
+    if(userInfo->loggedIn == 0)
+    {
+        printf("You are not logged in.");
+        return 0;
+    }
+    strcpy(mailboxPath, "../files/");
+    strcat(mailboxPath, userInfo->username);
+    strcat(mailboxPath, ".txt");
+    mailbox = fopen(mailboxPath, "r");
+    if(mailbox == NULL)
+    {
+        printf("Mailbox is either empty or unavailable at the moment. Please try again later.\n");
+        return 0;
+    }
+    else
+    {
+        while(fgets(content, 256, mailbox)) //reads the entire line(a message) from mailbox into content
+        {
+            printf("%s", content);
+            //send(userInfo->socket, content, strlen(content));
+            //delete file 
+        }
+    }
+    fclose(mailbox);
+    return 1;
+
+}
 
 
 
 void* recv_thread(void* args)
 {
 	//Receive a message from client
+    int i;
 	userData *userInfo = (userData*)args; 
 	int *client_sock = userInfo->socket;
 	char client_message[DEFAULT_BUFLEN] = "";
@@ -55,22 +210,33 @@ void* recv_thread(void* args)
         strtok(commandString, " ");
         printf("KOMANDA: '%s'\n", commandString);
         Command userCommand = stringToCommand(commandString);
-
+        //printf("userCommand = %s\nCommand = %d\n", commandString, userCommand);
         //Obrada zahteva
         switch(userCommand)
         {
         case LOGIN: //TODO PROVERE
+            handleLogin(client_message, userInfo);
+            printf("%s %d\n", userInfo->username, userInfo->loggedIn);
             break;
         case LOGOUT: 
+            handleLogout(userInfo);
             break;
         case SEND:
+            handleSend(client_message, userInfo);
             break;
         case CHECK:
+            handleCheck(userInfo);
             break;
         case RECEIVE:
+            handleReceive(userInfo);
             break;
         default:
+            //do nothing for exit
             break;
+        }
+        for(i = 0; i < DEFAULT_BUFLEN; i++)
+        {
+            client_message[i] = '\0';
         }
     }
 
@@ -132,6 +298,7 @@ int main(int argc , char *argv[])
         *client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
         userData userInfo;
         userInfo.socket = client_sock;
+        userInfo.loggedIn = 0;
         if (client_sock < 0)
         {
            perror("accept failed");
@@ -144,4 +311,3 @@ int main(int argc , char *argv[])
 
     return 0;
 }
-
